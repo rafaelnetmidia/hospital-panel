4670a6653247d3454590bb77f976519245893ed8
rafaelnetmidia <32550511+rafaelnetmidia@users.noreply.github.com>
2024-11-02 12:44:52 -0300

    projeto inicial criado pela arquitetura.

---
+++ src/main/java/br/com/hospital/painel/hospitalpanel/HospitalPanelApplication.java
@@ -0,0 +1,13 @@
+package br.com.hospital.painel.hospitalpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalPanelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalPanelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/hospital/painel/hospitalpanel/HospitalPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hospital.painel.hospitalpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalPanelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
